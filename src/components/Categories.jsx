ac9abcc16426667abc101b57999590070d3343e3
Sahan Thenuwara <t.a.s.sanjith@gmail.com>
2023-11-15 12:14:50 +0530

    added category component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+    ${mobile({ padding: "0px", flexDirection: "column" })}
+`;
+
+const Categories = () => {
+    return (
+        <Container>
+            {categories.map((item) => (
+                <CategoryItem item={item}></CategoryItem>
+            ))}
+        </Container>
+    );
+};
+
+export default Categories;
